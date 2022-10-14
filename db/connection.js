c81952079afd09e5eb071ab90a4fac957467024e
G Codes <graciela.mejia1@yahoo.com>
2022-10-14 11:21:21 -0700

    add schema.sql tables

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   user: "root",
   // Your MySQL password
   password: "Help_1234!",
-  database: "keepn-track",
+  database: "keepntrack",
 });
 
 db.connect((err) => {
