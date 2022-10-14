de634aadc8d9868a910d485a0f2991c8856b8fad
G Codes <graciela.mejia1@yahoo.com>
2022-10-14 10:56:05 -0700

    inital commit/add connection.js

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  // Your MySQL username,
+  user: "root",
+  // Your MySQL password
+  password: "Help_1234!",
+  database: "keepn-track",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  else console.log("YAY");
+});
+
+module.exports = db;
